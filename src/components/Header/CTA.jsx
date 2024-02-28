d403679b6045794430813d47cb85a49412f23009
Juan Francisco Schallibaum <jfschallibaum@gmail.com>
2024-02-28 10:40:51 -0300

    update portfolio

--- src/components/Header/CTA.jsx
+++ src/components/Header/CTA.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
-import CV from '../../assets/cv.pdf'
-import { BiDownload } from 'react-icons/bi'
+import React from "react";
+import CV1 from "../../assets/cv_es.pdf";
+import CV2 from "../../assets/cv_en.pdf";
+import { BiDownload } from "react-icons/bi";
 
 const CTA = () => {
   return (
     <div className="cta">
-      <a href={CV} download className='btn'>Download CV <BiDownload /></a>
+      <a href={CV1} download className="btn">
+        CV - ES
+        <BiDownload />
+      </a>
+      <a href={CV2} download className="btn">
+        CV - EN
+        <BiDownload />
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default CTA
+export default CTA;
